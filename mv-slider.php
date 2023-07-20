366f56352facd711af2e1e738ef9666a5f59c98c
TingSyuanWang <vincent@fishboneapps.com>
2023-07-20 12:09:52 -0400

    feat: prevent root access directly
    Signed-off-by: TingSyuanWang <vincent@fishboneapps.com>

--- mv-slider.php
+++ mv-slider.php
@@ -2,7 +2,7 @@
 
 /**
  * Plugin Name: MV Slider
- * Plugin URI: https://www.wordpress.org/mv-slider
+ * Plugin URI: https://github.com/TingSyuanWang/mv-slider
  * Description: A simple slider plugin for WordPress
  * Version: 1.0
  * Requires at least: 5.6
@@ -13,3 +13,13 @@
  * Text Domain: mv-slider
  * Domain Path: /languages
  */
+
+/*
+MV Slider is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 2 of the License, or any later version.
+MV Slider is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+You should have received a copy of the GNU General Public License along with MV Slider. If not, see https://www.gnu.org/licenses/gpl-2.0.html.
+*/
+
+if ( ! defined('ABSPATH') ) {
+	exit;
+}
