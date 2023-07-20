681aea17e60458ce0da57b64f79ced681be61ed7
TingSyuanWang <vincent@fishboneapps.com>
2023-07-20 14:20:48 -0400

    feat: add custom post types
    Signed-off-by: TingSyuanWang <vincent@fishboneapps.com>

--- mv-slider.php
+++ mv-slider.php
@@ -28,6 +28,9 @@ if( ! class_exists( 'MV_Slider' ) ) {
 	class MV_Slider {
 		function __construct() {
 			$this->define_constants();
+
+			require_once( MV_SLIDER_PATH . 'post-types/class.mv-slider-cpt.php' );
+			$MV_Slider_Post_Type = new MV_Slider_Post_Type();
 		}
 
 		public function define_constants(): void {
@@ -42,6 +45,7 @@ if( ! class_exists( 'MV_Slider' ) ) {
 
 		public static function deactivate(): void {
 			flush_rewrite_rules();
+			unregister_post_type( 'mv_slider' );
 		}
 
 		public static function uninstall() {
