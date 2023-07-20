54852183c15433c13149303ff958b7c5a72ab244
TingSyuanWang <vincent@fishboneapps.com>
2023-07-20 11:47:19 -0400

    feat: add plugin information
    Signed-off-by: TingSyuanWang <vincent@fishboneapps.com>

---
+++ mv-slider.php
@@ -0,0 +1,15 @@
+<?php
+
+/**
+ * Plugin Name: MV Slider
+ * Plugin URI: https://www.wordpress.org/mv-slider
+ * Description: A simple slider plugin for WordPress
+ * Version: 1.0
+ * Requires at least: 5.6
+ * Author: Vincent Wang
+ * Author URI: https://vincent.fishboneapps.com
+ * License: GPL v2 or later
+ * License URI: https://www.gnu.org/licenses/gpl-2.0.html
+ * Text Domain: mv-slider
+ * Domain Path: /languages
+ */
